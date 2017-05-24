584f0fbbfedff64163d83ecd7c37d5fdd5ef6b97
Piotr Bobinski <piotr.bobinski@ericsson.com>
2017-05-24 12:09:15 +0200

    Add credits to app

--- app/src/main/java/com/example/epiobob/pomodoroapp/MainActivity.java
+++ app/src/main/java/com/example/epiobob/pomodoroapp/MainActivity.java
@@ -6,13 +6,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -78,5 +78,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void goToCredits(MenuItem item) {
+        Intent intent = new Intent(this, CreditsActivity.class);
+        startActivity(intent);
     }
 }
